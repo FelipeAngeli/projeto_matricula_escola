383df9e98b9a14cfde4c32bf533513678c72075c
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2025-01-15 14:09:48 -0300

    adicionar configuração de autenticação e permissões no rest_framework

--- app/settings.py
+++ app/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuração do rest_framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
