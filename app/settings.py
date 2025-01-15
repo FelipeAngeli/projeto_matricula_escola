bb13cfd8e4c4616fda9a47c878e7ff6cd70ba437
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2025-01-15 16:28:38 -0300

    adicionar versionamento ao EstudanteViewSet e criar EstudanteSerializerV2; atualizar configurações de filtragem e pesquisa

--- app/settings.py
+++ app/settings.py
@@ -137,5 +137,8 @@ REST_FRAMEWORK = {
 
     #paginação
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+
+    #versionamento
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
 }
