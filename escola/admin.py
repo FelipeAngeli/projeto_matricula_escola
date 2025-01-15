2405bb269bcc200aea2912065ec5f5bb523e22d2
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2025-01-15 10:28:45 -0300

    serializers

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
-# Register your models here.
+
+class Estudantes(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'email', 'celular', 'cpf', 'data_nascimento']
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Estudante, Estudantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ['id', 'codigo', 'descricao', 'nivel']
+    list_display_links = ('id', 'codigo')
+    search_fields = ('codigo',)
+
+admin.site.register(Curso, Cursos)
